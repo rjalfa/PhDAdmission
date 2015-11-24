5320bd2f4fba97cae5516ff7558602fcc668c00a
Rounaq <rounaqwl66@gmail.com>
2015-11-24 20:27:10 +0530

    Refactor and some design changes INDEX2

--- WebContent/js/index.js
+++ WebContent/js/index.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
 	$.material.init();
+	var vdate = new Date();
+    vdate.setFullYear(vdate.getFullYear() - 15);
+    $("#dateOfBirthField").bootstrapMaterialDatePicker({ weekStart : 0, time: false ,maxDate: vdate});
+    $("#adfField").bootstrapMaterialDatePicker({ weekStart : 0, time: false});
+    $("#adtField").bootstrapMaterialDatePicker({ weekStart : 0, time: false});
 });
